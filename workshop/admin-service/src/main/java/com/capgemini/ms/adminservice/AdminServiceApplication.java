39a476cf7fd64df2e54b6d54855793c48b06e462
Krzysztof Sobkowiak <krzys.sobkowiak@gmail.com>
2017-07-04 10:25:13 +0200

    Projects for infrastructure services generated

---
+++ workshop/admin-service/src/main/java/com/capgemini/ms/adminservice/AdminServiceApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.ms.adminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServiceApplication.class, args);
+	}
+}
