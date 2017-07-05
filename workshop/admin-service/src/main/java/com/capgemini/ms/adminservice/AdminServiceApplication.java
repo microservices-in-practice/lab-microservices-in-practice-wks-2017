e4de88d8e94d7d9972e5b9e4ebc4c7e607e1c5d5
Krzysztof Sobkowiak <krzys.sobkowiak@gmail.com>
2017-07-05 10:22:53 +0200

    Infrastructure services implemented

--- workshop/admin-service/src/main/java/com/capgemini/ms/adminservice/AdminServiceApplication.java
+++ workshop/admin-service/src/main/java/com/capgemini/ms/adminservice/AdminServiceApplication.java
@@ -1,9 +1,22 @@
 package com.capgemini.ms.adminservice;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.retry.annotation.EnableRetry;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableAdminServer
+@EnableRetry
+@EnableTurbine
 public class AdminServiceApplication {
 
 	public static void main(String[] args) {
