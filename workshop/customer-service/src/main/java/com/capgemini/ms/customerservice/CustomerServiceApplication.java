5fdee2fac1917c90a6b709090936b020370a1644
Krzysztof Sobkowiak <krzys.sobkowiak@gmail.com>
2017-07-06 08:13:09 +0200

    Customer and catalog services binded with infra

--- workshop/customer-service/src/main/java/com/capgemini/ms/customerservice/CustomerServiceApplication.java
+++ workshop/customer-service/src/main/java/com/capgemini/ms/customerservice/CustomerServiceApplication.java
@@ -2,7 +2,11 @@ package com.capgemini.ms.customerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+@EnableHystrix
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CustomerServiceApplication {
 
